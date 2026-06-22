28b6f9f9bb9e294c6bd641e1f2d8349ce98d6826
Ice-rink <15551057621@163.com>
2026-06-22 10:56:24 +0000

    feat: 支持私聊和生成图片工具导出

--- sb3_AIGC-Image/index.js
+++ sb3_AIGC-Image/index.js
@@ -9,32 +9,82 @@ const config = {
     key: "sk-000000000000000000000000000000000000000000000000", // 请求密钥
     url: "https://apihub.agnes-ai.com/v1/images/generations", // 请求的ai端点
     model: "agnes-image-2.1-flash", // 图片生成大模型
-    timeout: 30000 // 等待时长
+    timeout: 400000 // 等待时长
 };
 
-// send msg
+// 群聊
 spark.on('message.group.normal', async (pack, reply) => {
     if (!((config.QQGroup.has("all")
         || config.QQGroup.has(pack.group_id))
         && pack.raw_message.startsWith("/生图 ")
     )) return;
 
+    onMessage(pack, reply);
+})
+
+// 私聊
+spark.on('message.private.friend', (pack, reply) => {
+    if (!((config.QQPrivate.has("all")
+        || config.QQPrivate.has(pack.target_id))
+        && pack.raw_message.startsWith("/生图 ")
+    )) return;
+
+    onMessage(pack, reply);
+});
+
+async function onMessage(pack, reply) {
     reply("收到！正在努力绘画中~");
 
+    // 提示词
+    const prompt = (pack.message.map(t => {
+        if (t.type === "text")
+            return t.data.text;
+    })).join(",").slice(4);
+
+    // 图片列表
+    const images = (pack.message.map(t => {
+        if (t.type === "image")
+            return t.data.url
+    })).filter(i => i != null);
+
+    sendImage(null, pack, prompt, images, reply)
+}
+
+// 导出AI调用工具
+spark.on("event.aichat.starts", () => {
+    spark.emit("event.aichat.add_tools", "generate_images", {
+        definition: {
+            type: "function",
+            function: {
+                description: "文生图片接口，生成完成后会自动发送，如需图生图请引导用户使用 '/生图 <prompt>'指令",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        prompt: {
+                            type: "string",
+                            description: "生图提示词",
+                        },
+                    },
+                    required: ["prompt"]
+                }
+            }
+        },
+        call: (chatData, prompt) => {
+            sendImage(chatData.uid, chatData.pack, prompt);
+            return "图片生成请求已发送，生成完毕后会自动发送至请求群聊";
+        }
+    })
+})
+
+async function sendImage(uid, pack, prompt, images = [], reply = null) {
+    if (reply === null) {
+        if (uid.startsWith("target_"))
+            reply = (...msg) => spark.QClient.sendPrivateMsg(uid.slice(7), ...msg);
+        else
+            reply = (...msg) => spark.QClient.sendGroupMsg(uid, ...msg);
+    }
+
     try {
-
-        // 提示词
-        const prompt = (pack.message.map(t => {
-            if (t.type === "text")
-                return t.data.text;
-        })).join(",").slice(4);
-
-        // 图片列表
-        const images = (pack.message.map(t => {
-            if (t.type === "image")
-                return t.data.url
-        })).filter(i => i != null);
-
         const data = {
             model: config.model,
             prompt: prompt,
@@ -43,8 +93,6 @@ spark.on('message.group.normal', async (pack, reply) => {
             },
         };
         if (images.length > 0) data.extra_body["image"] = [...images];
-
-        console.warn("QQ -> AI: ", JSON.stringify(data, null, 4));
 
         const res = await axios.post(config.url,
             data,
@@ -57,23 +105,15 @@ spark.on('message.group.normal', async (pack, reply) => {
             }
         );
 
-        // console.warn("res:\n" + (JSON.stringify(res, (key, value) => {
-        //     if (key === 'request' || key === 'config' || key === 'headers') return undefined;
-        //     if (typeof value === 'bigint') return value.toString();
-        //     return value;
-        // }, 4)));
-
         reply([
             msgbuilder.reply(pack.real_id),
-            msgbuilder.at(pack.user_id),
             msgbuilder.text(" 画完啦～"),
             msgbuilder.img(res.data.data[0].url)
         ]);
     } catch (err) {
         reply([
             msgbuilder.reply(pack.real_id),
-            msgbuilder.at(pack.user_id),
             msgbuilder.text(`画不出来，怎么样都画不出来！>n<\n\n>> ${err}`),
         ]);
     }
-})
+}
